efe0f4af85f7e30d5cf4278a2a057517f83245a2
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-24 03:56:41 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -83,6 +83,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
+  matrix = []
   matrix = assembled_matrix
   martix[0][2] = "Lemons"
   # Given any matrix (array of arrays), a row index and a column index, 
