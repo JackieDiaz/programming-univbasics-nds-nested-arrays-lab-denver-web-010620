c19eeffdcbfca687015b089956d09cd4d2e4dc83
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-24 04:06:43 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -84,6 +84,7 @@ end
 
 def matrix_update(matrix, row, column, new_value)
 local_matrix = assembled_matrix
+local_matrix[2] = "Lemons"
  
   # updated_matrix
   # Given any matrix (array of arrays), a row index and a column index, 
