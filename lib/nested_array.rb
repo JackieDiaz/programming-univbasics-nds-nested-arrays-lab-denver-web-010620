01eb31082fef063a81517c5186a1be6a0e5eff11
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-24 05:29:14 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,25 +17,68 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+produce_storage = 
+ [
+  "Grapefruit",
+  "Pineapple",
+  "Oranges",
+  "Watermelon",
+  "Eggplant"
+],
+[
+  "Strawberries",
+  "Potatoes",
+  "Grapes",
+  "Avocadoes",
+  "Asparagus"
+]
 end
 
 def sorted_matrix
+   produce_storage = 
+ conventional_produce = [
+  "Grapefruit",
+  "Pineapple",
+  "Oranges",
+  "Watermelon",
+  "Eggplant"
+],
+organic_produce =[
+  "Strawberries",
+  "Potatoes",
+  "Grapes",
+  "Avocadoes",
+  "Asparagus"
+]
+sorted_conventional_produce = conventional_produce.sort
+sorted_organic_produce = organic_produce.sort
+produce_storage = sorted_conventional_produce , sorted_organic_produce
+produce_storage
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
 
 def matrix_lookup(matrix, row, column)
+ martrix = []
+ matrix[1]
+ matrix[1][1]
+ 
+  # matrix_find = martix[1],[1]
+  # row_find = row[1],[1]
+  # column_find = column[1],[1]
+  # matrix_find
+  # row_find
+  # column_find
+  
+  
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+martix = assembled_matrix
+matrix[0][2] = new_value
+matrix
+
 end
